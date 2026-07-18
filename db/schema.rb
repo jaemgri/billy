5f14be12092c031b182798ce02c15d2149eba5c3
Nina Galindo <ninalorettagalindo@gmail.com>
2026-07-18 16:24:26 +0900

    added the Chat model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_18_071908) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_18_072220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,13 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_18_071908) do
     t.string "name"
     t.date "received_date"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "chats", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_chats_on_user_id"
   end
 
   create_table "shared_bills", force: :cascade do |t|
@@ -46,6 +53,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_18_071908) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "chats", "users"
   add_foreign_key "shared_bills", "bills"
   add_foreign_key "shared_bills", "users"
 end
