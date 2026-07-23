09198effdd1cb7b9766f892e7572f23fec7bf0eb
James Grigson <jamesgrigson15@gmail.com>
2026-07-23 18:09:07 +0900

    Add chat list, chat show, start-a-chat, fix User associations, add user_id to bills

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :shared_bills
-  has_many :bills, through: :shared_bills
+
+  has_many :bills, dependent: :destroy
+  has_many :shared_bills, dependent: :destroy
+  has_many :shared_with_bills, through: :shared_bills, source: :bill
+  has_many :chats, dependent: :destroy
 end
