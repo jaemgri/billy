9521602dd388a0b0bf3ef3414604750011becdd2
James Grigson <jamesgrigson15@gmail.com>
2026-07-23 18:27:54 +0900

    Fix bill.title to bill.name in chat views and add bills to chats

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,6 +1,8 @@
 class Bill < ApplicationRecord
+  belongs_to :user
   has_many :shared_bills, dependent: :destroy
   has_many :users, through: :shared_bills
+  has_many :chats, dependent: :destroy
 
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,5 @@
 class Chat < ApplicationRecord
   belongs_to :user
+  belongs_to :bill
   has_many :messages, dependent: :destroy
 end
