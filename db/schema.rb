2db999daa64866b5136c4ad0af3c6c010903f5f5
Cassandra Santoso <cassandra.santoso04@gmail.com>
2026-07-27 14:32:37 +0900

    migration to remove roles from db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_27_050640) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_27_053016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -78,7 +78,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_27_050640) do
     t.datetime "created_at", null: false
     t.string "invited_email"
     t.boolean "paid"
-    t.string "role"
     t.decimal "split_amount"
     t.string "status", default: "pending", null: false
     t.datetime "updated_at", null: false
